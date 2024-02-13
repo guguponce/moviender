5d27151cd9acde548766468bc88a31d44823a0f2
guguponce <augustoponceph@gmail.com>
2024-02-13 17:11:31 +0100

    new configs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,21 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+    colors: {
+      astronaut: {
+        50: "#E9EDF7",
+        100: "#D2DBEF",
+        200: "#A9B9E0",
+        300: "#7C95CF",
+        400: "#5071BF",
+        500: "#39579D",
+        600: "#293E70",
+        700: "#1F3056",
+        800: "#151F38",
+        900: "#0B111E",
+        950: "#05080F",
+      },
+    },
   },
   plugins: [],
 };
